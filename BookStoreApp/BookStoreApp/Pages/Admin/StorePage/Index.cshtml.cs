3ecefd895dabd634525db3b60621d0ff82618404
Tran Thanh Dat <97006331+DatTranLK@users.noreply.github.com>
2022-10-17 16:24:10 +0700

    Dat: Add New DB

---
+++ BookStoreApp/BookStoreApp/Pages/Admin/StorePage/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookStoreApp.Pages.Admin.StorePage
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
