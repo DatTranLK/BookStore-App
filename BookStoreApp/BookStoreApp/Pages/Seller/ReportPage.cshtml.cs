c85d35a943e187f7b3df6334b055867ae2c9d655
thangtn2101 <thangn.tran2101@gmail.com>
2022-10-23 19:33:26 +0700

    Thang: Complete view in seller

---
+++ BookStoreApp/BookStoreApp/Pages/Seller/ReportPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookStoreApp.Pages.Seller
+{
+    public class ReportPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
